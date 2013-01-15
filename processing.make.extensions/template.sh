d3995bfb12748fa6bb8e814c6ac331020205fde8
Paul Vollmer <wrongentertainment@yahoo.de>
2013-01-15 10:29:54 +0100

    create processing.make.extensions and template

---
+++ processing.make.extensions/template.sh
@@ -0,0 +1,16 @@
+#!bin/sh
+
+#
+# template.sh
+# 
+# Template shell file/placeholder.
+#
+#
+# This file is part of the processing.make project
+# version 0.1.0
+#
+
+echo ""
+echo "template.sh Hello World"
+echo "Here you can place shell/bash script."
+echo ""
